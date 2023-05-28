131a3c6f3c167239c9672e030788a218f2a26ad6
Luis Melo <ascarixs@gmail.com>
2023-05-27 21:27:16 -0300

    Loading - Added uiball loader

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,10 +1,12 @@
 import { LoadingImage } from './LoadingImage'
+import { DotPulse } from '@uiball/loaders'
 
 export function Loading() {
   return (
-    <section className="flex">
+    <section className="flex items-center gap-4">
       <LoadingImage />
-      Cargando...
+      Cargando
+      <DotPulse size={40} speed={1.3} color="#B8B8B8" />
     </section>
   )
 }
